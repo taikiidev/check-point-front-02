1c1ce1d4871d18d7e6f2919674e69b5331119217
Taiki Mello <143201421+taikiidev@users.noreply.github.com>
2023-10-27 08:26:45 -0300

    Add files via upload

---
+++ series.js
@@ -0,0 +1,12 @@
+
+// Criando constante de array com as minhas 3 series favoritas.
+const my3FavoriteShows = ['Dark','Cyberpunk Edgerunners','Better Call Saul']
+// Iniciando estrutura if-else para verificar se o tamanho do array é igual a 3.]
+
+if (my3FavoriteShows.length == 3) {
+    // Se sim, printar True
+    console.log('True')
+} else {
+    // Se nao, printar False
+    console.log('False')
+}
